4d0353ffa22c952481593732f804b8958303fd0d
Jakob Persons <jakobep24@gmail.com>
2021-05-21 13:58:04 -0400

    user controller change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
 
   # add action methods(index, show, create)
+  # //GET /users
+  def index
+    user = User.first
+    render json: user
+  end
+  
+
 end
