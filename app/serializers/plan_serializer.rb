e52851f58891fd16c18f1af37e7894e68ef06298
Jakob Persons <jakobep24@gmail.com>
2021-05-25 14:42:48 -0400

    add current to plan serializer

--- app/serializers/plan_serializer.rb
+++ app/serializers/plan_serializer.rb
@@ -1,5 +1,5 @@
 class PlanSerializer < ActiveModel::Serializer
-  attributes :id, :app_target, :net_target, :user_id
+  attributes :id, :app_target, :net_target, :current_app, :current_net, :user_id
 
   has_many :assessments
   has_many :reflections
