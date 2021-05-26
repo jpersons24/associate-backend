b4ad5f255230eec4b47c1c969a926088c20eddfb
Jakob Persons <jakobep24@gmail.com>
2021-05-26 00:23:08 -0400

    add params to update action, plans controller

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,9 +1,19 @@
 class PlansController < ApplicationController
 
+  def index
+    plans = Plan.all 
+    render json: plans
+  end
+
   # /PATCH/plans/:id
   def update
     plan = Plan.find(params[:id])
-    plan.update(app_target: params[:app_target], net_target: params[:net_target], user_id: params[:user_id])
+    plan.update(
+      app_target: params[:app_target], 
+      net_target: params[:net_target], 
+      user_id: params[:user_id], 
+      current_app: params[:current_app], 
+      current_net: params[:current_net])
     render json: plan
   end
   
